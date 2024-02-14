a768c1d8aa83a1216513e032afc1a866be3d4f9c
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-02-14 10:42:37 +0100

    feat: country scroll tag

--- src/App.jsx
+++ src/App.jsx
@@ -27,14 +27,28 @@ function App() {
         className=" placeholder:ml-10 w-80 h-10 rounded-xl bg-slate-50 border-2 border-black "
       />
 
-      <div className="grid grid-cols-5 items-start gap-8 m-5">
+      <div className="grid grid-cols-5 mt-20 items-start gap-8 m-5">
         {country?.map((item) => (
           <div
-            className="flex flex-col items-start justify-start "
+            className="flex flex-col items-start  overflow-x-clip  justify-start gap-2 "
             key={item.cca3}
           >
-            <img src={item.flags.png} alt={`a flag of ${item.name.common}`} />
-            <p>Country: {item.name.official}</p>
+            <img
+              src={item.flags.png}
+              className="h-32"
+              alt={`a flag of ${item.name.common}`}
+            />
+            <p className="flex gap-2 items-center justify-center">
+              Country:
+              <div className="flex w-80 overflow-x-scroll no-scrollbar">
+                <p className="flex   flex-nowrap">
+                  <span className="w-[450px] flex overflow-x-scroll  flex-nowrap">
+                    {item.name.official}
+                  </span>
+                </p>
+              </div>
+            </p>
+
             <p>Capital: {item?.capital}</p>
             <p>Population: {item.population}</p>
             <p>{item.independent ? "Independent" : "Dependent"}</p>
