349de91bd92444af8721ea9bfb012c37f6aa8212
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-02-15 13:16:38 +0100

    created loader skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Country from "./pages/Country";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <div className=" w-full overflow-x-hidden">
       <NavBar />
       <Routes>
-        <Route path="/country" element={<Country/>} />
-
+        <Route path="/country" element={<Country />} />
+        <Route path="test" element={<Test />} />
       </Routes>
     </div>
   );
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,19 +1,39 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Country = () => {
   const [country, setCountries] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [input, setInput] = useState("");
 
+  //   useEffect(() => {
+  //     fetch(`https://restcountries.com/v3.1/${input ? `/name/${input}` : "all"} `)
+  //       .then((res) => res.json())
+  //       .then((res) => {
+  //         let country = res;
+  //         setCountries(country);
+  //       })
+  //       .catch((err) => setError(err));
+  //   }, [input]);
+
   useEffect(() => {
-    fetch(`https://restcountries.com/v3.1/${input ? `/name/${input}` : "all"} `)
-      .then((res) => res.json())
+    setLoading(true);
+    axios
+      .get(
+        `https://restcountries.com/v3.1/${input ? `/name/${input}` : "all"} `
+      )
       .then((res) => {
-        let country = res;
+        let country = res.data;
+        console.log(res.data);
         setCountries(country);
+        setLoading(true);
       })
       .catch((err) => setError(err));
   }, [input]);
+
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <div>
       <input
@@ -24,32 +44,39 @@ const Country = () => {
       />
 
       <div className="grid grid-cols-5 mt-20 items-start gap-8 m-5">
-        {country?.map((item) => (
-          <div
-            className="flex flex-col items-start  overflow-x-hidden  justify-start gap-2 "
-            key={item.cca3}
-          >
-            <img
-              src={item.flags.png}
-              className="h-32 w-full border border-slate-400"
-              alt={`a flag of ${item.name.common}`}
-            />
-            <p className="flex gap-2 items-center justify-center">
-              Country:
-              <div className="flex w-80 overflow-x-scroll no-scrollbar">
-                <p className="flex   flex-nowrap">
-                  <span className="w-[400px] flex overflow-x-scroll  flex-nowrap">
-                    {item.name.official}
-                  </span>
+        {loading
+          ? arr.map((index) => (
+              <div
+                key={index}
+                className="h-40 bg-slate-300 rounded-md animate-pulse"
+              ></div>
+            ))
+          : country?.map((item) => (
+              <div
+                className="flex flex-col items-start  overflow-x-hidden  justify-start gap-2 "
+                key={item.cca3}
+              >
+                <img
+                  src={item.flags.png}
+                  className="h-32 w-full border border-slate-400"
+                  alt={`a flag of ${item.name.common}`}
+                />
+                <p className="flex gap-2 items-center justify-center">
+                  Country:
+                  <div className="flex w-80 overflow-x-scroll no-scrollbar">
+                    <p className="flex   flex-nowrap">
+                      <span className="w-[400px] flex overflow-x-scroll  flex-nowrap">
+                        {item.name.official}
+                      </span>
+                    </p>
+                  </div>
                 </p>
-              </div>
-            </p>
 
-            <p>Capital: {item?.capital}</p>
-            <p>Population: {item.population}</p>
-            <p>{item.independent ? "Independent" : "Dependent"}</p>
-          </div>
-        ))}
+                <p>Capital: {item?.capital}</p>
+                <p>Population: {item.population}</p>
+                <p>{item.independent ? "Independent" : "Dependent"}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
